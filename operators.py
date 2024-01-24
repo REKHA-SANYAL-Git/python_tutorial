c3c65764ec7afb218e651d100275a6ec45b146f9
REKHA-SANYAL-Git <sanyalrekha0101@gmail.com>
2024-01-24 19:47:39 +0530

    operators page

---
+++ operators.py
@@ -0,0 +1,65 @@
+# Operators are used to perform operations on variables and values.
+
+# ***Arithmetic Operators***
+
+# Addition
+print(10 + 5)
+
+# Subtraction
+print(10 - 5)
+
+# Multiplication
+print(10 * 5)
+
+# Division
+print(10 / 5)
+
+# Modulus
+print(10 % 4)
+
+# Exponentiation
+print(10 ** 5)
+
+# Floor division
+print(10 // 5)
+
+# ***Assignment Operators***
+a = 5
+
+print(a)
+
+b = 6
+b += 3
+print(b)
+
+c= 6
+c -= 3
+print(c)
+
+d = 6
+d *= 3
+print(d)
+
+e = 6
+e /= 3
+print(e)
+
+f = 6
+f %= 3
+print(f)
+
+g = 6
+g **= 3
+print(g)
+
+h = 6
+h //= 3
+print(h)
+
+i = 6
+i &= 3
+print(i)
+
+j = 6
+j |= 3
+print(j)
